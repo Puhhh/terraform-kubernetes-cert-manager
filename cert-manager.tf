34777f12b6bd5c0a4b8c4a13827cfc25b9d739d8
Aleksei Blinov <a.blin0v@mail.ru>
2024-07-25 13:22:10 +0300

    terraform-kubernetes-cert-manager

---
+++ cert-manager.tf
@@ -0,0 +1,15 @@
+resource "kubernetes_namespace" "cert-manager-namespace" {
+  metadata {
+    name = var.cert-manager-namespace
+  }
+}
+
+resource "helm_release" "cert-manager" {
+  namespace  = kubernetes_namespace.cert-manager-namespace.metadata[0].name
+  name       = var.helm-name
+  chart      = var.helm-chart-name
+  repository = var.helm-chart-repo
+  version    = var.helm-chart-version
+
+  values = var.helm-custom-values ? [file("${var.helm-custom-values-path}")] : []
+}
---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "kubeconfig-path" {
+  description = "Kubeconfig Path"
+  type        = string
+  default     = "~/.kube/config"
+}
+
+variable "cert-manager-namespace" {
+  description = "Cert Manager Namespace"
+  type        = string
+  default     = "cert-manager"
+}
+
+variable "helm-name" {
+  description = "Helm Release Name"
+  type        = string
+  default     = "cert-manager"
+}
+
+variable "helm-chart-name" {
+  description = "Helm Chart Name"
+  type        = string
+  default     = "cert-manager"
+}
+
+variable "helm-chart-repo" {
+  description = "Helm Chart Repo"
+  type        = string
+  default     = "https://charts.jetstack.io/"
+}
+
+variable "helm-chart-version" {
+  description = "Helm Chart Version"
+  type        = string
+  default     = "1.15.1"
+}
+
+variable "helm-custom-values" {
+  description = "Use Helm Custom Values"
+  type        = bool
+  default     = false
+}
+
+variable "helm-custom-values-path" {
+  description = "Helm Custom Values Path"
+  type        = string
+  default     = ""
+}
