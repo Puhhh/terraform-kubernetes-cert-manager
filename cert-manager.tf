4984f6da9c59c597a84beafbbf50a0871aa4051e
Aleksei Blinov <alvablinov@gmail.com>
2024-09-03 16:52:51 +0300

    tflint issues fix

--- cert-manager.tf
+++ cert-manager.tf
@@ -11,5 +11,5 @@ resource "helm_release" "cert-manager" {
   repository = var.helm-chart-repo
   version    = var.helm-chart-version
 
-  values = var.helm-custom-values ? [file("${var.helm-custom-values-path}")] : []
+  values = var.helm-custom-values ? [file(var.helm-custom-values-path)] : []
 }
